ccfd9d33bae6bf8e55805fe72674400fb26377cf
Denison TS <106643117+denisonts@users.noreply.github.com>
2022-08-16 00:37:23 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraConversao/ConversorDeMoeda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConversao
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Dolar;
+        public static double Quantidade;
+        public static double Iof = 6.0;
+
+        public static double Conv() {
+            return Dolar * Quantidade;
+        }
+        
+        public static double Total() {
+            double soma = Dolar * Quantidade;
+            return soma + soma * Iof / 100.0;
+        }
+    }
+}
---
+++ CalculadoraConversao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CalculadoraConversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dolar? ");
+            ConversorDeMoeda.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                    
+            
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.Total();
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
